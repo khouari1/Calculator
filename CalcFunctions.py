78a341c40876c304bb3fcc1bb96b7fc699c57128
Karim Houari <karimhouari@karims-mbp.home>
2014-06-04 17:37:59 +0100

    Edited to validate user inputs where required

--- CalcFunctions.py
+++ CalcFunctions.py
@@ -1,24 +1,39 @@
 import decimal
 
+run_list = ["yes", "y", "agree"]
+
+#def verify(x):
+#  while True:
+#    try:
+#      decimal.Decimal(x)
+#      break
+#    except decimal.InvalidOperation:
+#      print ("Invalid input.")
 def add(x, n):
   total = 0
+  x = decimal.Decimal(x)
+  n = decimal.Decimal(n)
   total = x + n
   return total
 def subtract(x, n):
   total = 0
+  x = decimal.Decimal(x)
+  n = decimal.Decimal(n)
   total = x - n
   return total
 def multiply(x, n):
   total = 0
+  x = decimal.Decimal(x)
+  n = decimal.Decimal(n)
   total = x * n
   return total
 def divide(x, n):
   total = 0
+  x = decimal.Decimal(x)
+  n = decimal.Decimal(n)
   total = x / n
   return total
-def runAgain():
-  runAgain = raw_input("Run again?")
-  if runAgain == ("yes"):
-    runAgain = True
-  else:
-    runAgain = False
+#def runAgain():
+#  runAgain = raw_input("Run again?").lower()
+#  if runAgain not in run_list:
+#    start = False
--- calculator.py
+++ calculator.py
@@ -1,35 +1,83 @@
 import CalcFunctions
 import decimal
 
-signs = ['+','-','*','/']
-runAgain = True
+signs = ['1','2','3','4']
+run_list = ["yes", "y", "agree"]
 
-while runAgain == True:
-    userInput = decimal.Decimal(raw_input("Enter number:"))
-    signsInput = raw_input("Choose your operation (+, -, *, /)")
-    if signsInput == signs[0]:
-      while True:
-        addInput = decimal.Decimal(raw_input("Enter number:"))
+while True:
+  print ("Choose your operation:")
+  print ("1: Addition")
+  print ("2: Subtraction")
+  print ("3: Multiplication")
+  print ("4: Division")
+  signsInput = raw_input()
+  if signsInput == signs[0]:
+    while True:
+      addInput1 = raw_input("Enter first number:")
+      try:
+        decimal.Decimal(addInput1)
         break
-      print CalcFunctions.add(userInput, addInput)
-      runAgain()
-    elif signsInput == signs[1]:
-      while True:
-        subtractInput = decimal.Decimal(raw_input("Enter number:"))
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    while True:
+      addInput2 = raw_input("Enter second number:")
+      try:
+        decimal.Decimal(addInput2)
         break
-      print CalcFunctions.subtract(userInput, subtractInput)
-      runAgain()
-    elif signsInput == signs[2]:
-      while True:
-        multiplyInput = decimal.Decimal(raw_input("Enter number:"))
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    print CalcFunctions.add(addInput1, addInput2)
+    break
+  elif signsInput == signs[1]:
+    while True:
+      subtractInput1 = raw_input("Enter first number:")
+      try:
+        decimal.Decimal(subtractInput1)
         break
-      print CalcFunctions.multiply(userInput, multiplyInput)
-      runAgain()
-    elif signsInput == signs[3]:
-      while True:
-        divideInput = decimal.Decimal(raw_input("Enter number:"))
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    while True:
+      subtractInput2 = raw_input("Enter second number:")
+      try:
+        decimal.Decimal(subtractInput2)
         break
-      print CalcFunctions.divide(userInput, divideInput)
-      runAgain()
-    else:
-      print ("Invalid input. Try again:")
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    print CalcFunctions.subtract(subtractInput1, subtractInput2)
+    break
+  elif signsInput == signs[2]:
+    while True:
+      multiplyInput1 = raw_input("Enter first number:")
+      try:
+        decimal.Decimal(multiplyInput1)
+        break
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    while True:
+      multiplyInput2 = raw_input("Enter second number:")
+      try:
+        decimal.Decimal(multiplyInput2)
+        break
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    print CalcFunctions.multiply(multiplyInput1, multiplyInput2)
+    break
+  elif signsInput == signs[3]:
+    while True:
+      divideInput1 = raw_input("Enter first number:")
+      try:
+        decimal.Decimal(divideInput1)
+        break
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    while True:
+      divideInput2 = raw_input("Enter second number:")
+      try:
+        decimal.Decimal(divideInput2)
+        break
+      except decimal.InvalidOperation:
+        print ("Invalid input.")
+    print CalcFunctions.divide(divideInput1, divideInput2)
+    break
+  else:
+    print ("Invalid input. Try again:")
